b8e13c32a5023efc03a44bab384179c7e5e7593c
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-23 03:55:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |element|
-    print "Hello " + element +"!"
+    puts  "Hello " + element +"!"
   end
 end
 
