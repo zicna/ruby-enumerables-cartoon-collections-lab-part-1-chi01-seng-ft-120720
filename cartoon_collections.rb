0bce22a1b9cfbaf68d6476039d51f31addd39615
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-23 04:05:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   new_arr = []
   array.each_with_index do |item, index|
-    new_arr.push(index, element)
+    new_arr.push(index, item)
   end
   puts new_arr
 end
